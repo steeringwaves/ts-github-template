ee9ee4e44430fa143be3e9d28d3cc7359ab3c2c4
aeaton <aeaton@batswireless.com>
2022-09-27 09:40:44 -0400

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -152,12 +152,10 @@ module.exports = {
 	// testLocationInResults: false,
 
 	// The glob patterns Jest uses to detect test files
-	testMatch: ["**/*.test.js"]
+	testMatch: ["**/*.test.js"],
 
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-	// testPathIgnorePatterns: [
-	//   "/node_modules/"
-	// ],
+	testPathIgnorePatterns: ["/node_modules/", "/dist/"]
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
 	// testRegex: [],
